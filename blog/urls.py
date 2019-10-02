6157a39ceb95386550c1af5ce22c1c7908828b5f
Evgeny <777koba@mail.ru>
2019-10-02 09:34:51 +0300

    New form for Posts

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('post/', views.post_trust),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     url(r'^$', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForms
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +13,6 @@ def post_detail(request, pk):
     post =get_object_or_404(Post, pk=pk)
     # post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+def post_new(request):
+    form = PostForms()
+    return render(request, 'blog/Forms.html', {'form':form} )
